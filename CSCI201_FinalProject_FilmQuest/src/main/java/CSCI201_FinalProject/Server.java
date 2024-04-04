1cc34d4bec6a8017921f8c82ce0e44c14849ffd0
anchals7 <anchalsr@usc.edu>
2024-04-03 20:15:15 -0700

    SQL Connection

--- CSCI201_FinalProject_FilmQuest/src/main/java/CSCI201_FinalProject/Server.java
+++ CSCI201_FinalProject_FilmQuest/src/main/java/CSCI201_FinalProject/Server.java
@@ -1,17 +1,36 @@
 package CSCI201_FinalProject;
 
 import java.net.ServerSocket;
-import java.sql.Connection;
 import java.util.List;
+import java.sql.*;
+
 
 public class Server {
 	private ServerSocket serverSocket;
 	private List<ServerThread> serverThreads;
-	private Connection connection = null;
+	private static Connection connection = null;
 	
 	//add connections for data base
 	public static void main(String [] args) {
 		//connect with sql database using jdbm
+		Statement st = null;
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/csci201_final_proj?user=root&password=root");
+			st = connection.createStatement();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("Connection not established");
+		}
+		
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
+			System.out.println("Error closing connection");
+		}
 	}
 	
 	public void getDatabase() {
